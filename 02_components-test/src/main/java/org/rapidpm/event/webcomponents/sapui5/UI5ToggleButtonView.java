c0e53f5a1136ee049c2f3aca6afa8bbcd3506feb
Daniel Nordhoff-Vergien <dve@vergien.net>
2019-09-27 16:06:00 +0200

    Add toggle button

--- 02_components-test/src/main/java/org/rapidpm/event/webcomponents/sapui5/UI5ToggleButtonView.java
+++ 02_components-test/src/main/java/org/rapidpm/event/webcomponents/sapui5/UI5ToggleButtonView.java
@@ -1,16 +1,25 @@
 package org.rapidpm.event.webcomponents.sapui5;
 
+import static org.rapidpm.event.webcomponents.sapui5.UI5ToggleButtonView.NAV;
+import static org.rapidpm.event.webcomponents.sapui5.UI5VersionDefinitions.UI5_VERSION;
+import org.rapidpm.dependencies.core.logger.HasLogger;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
-import org.rapidpm.dependencies.core.logger.HasLogger;
-
-import static org.rapidpm.event.webcomponents.sapui5.UI5ToggleButtonView.NAV;
 
 @Route(NAV)
-public class UI5ToggleButtonView
-    extends Composite<Div>
-    implements HasLogger {
+@NpmPackage(value = "@ui5/webcomponents", version = UI5_VERSION)
+@JsModule("@ui5/webcomponents/dist/ToggleButton.js")
+public class UI5ToggleButtonView extends Composite<Div> implements HasLogger {
   public static final String NAV = "UI5ToggleButtonView";
 
+  public UI5ToggleButtonView() {
+    UI5ToggleButton toggleButton = new UI5ToggleButton("Toggle me");
+
+    toggleButton.addValueChangeListener(
+        event -> System.out.println("ValueChangeEvent: " + event.getValue()));
+    getContent().add(toggleButton);
+  }
 }
